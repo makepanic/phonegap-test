afdad0eaafbe61d47d516d70cb32126394fb7348
Christian <make.panic@googlemail.com>
2013-04-22 14:08:24 +0200

    changes

--- js/app.js
+++ js/app.js
@@ -20,6 +20,35 @@ provides:
 
 if (!window.ViewController) window.ViewController = {};
 
+ViewController.Test = new Class({
+    Extends: Moobile.ViewController,
+
+    testButton : null,
+
+    loadView: function(){
+        this.view = Moobile.ScrollView.at('templates/views/test-view.html');
+    },
+
+    viewDidLoad: function(){
+        this.testButton = this.view.getChildComponent('test-button');
+        this.testButton.addEvent('tap', this.bound('onTestButtonTap'));
+    },
+
+    destroy: function() {
+        this.testButton.removeEvent('tap', this.bound('onTestButtonTap'));
+        this.testButton = null;
+        this.parent();
+    },
+
+    onTestButtonTap: function(e, sender){
+        var alert = new Moobile.Alert();
+        alert.setTitle('Hi');
+        alert.setMessage('Welcome to Moobile!');
+        alert.showAnimated();
+        this.getViewControllerStack().popViewController();
+    }
+});
+
 ViewController.Home = new Class({
 
 	Extends: Moobile.ViewController,
@@ -43,10 +72,7 @@ ViewController.Home = new Class({
 	},
 
 	onHelloButtonTap: function(e, sender) {
-		var alert = new Moobile.Alert();
-		alert.setTitle('Hi');
-		alert.setMessage('Welcome to Moobile!');
-		alert.showAnimated();
+        this.getViewControllerStack().pushViewController(new ViewController.Test, new Moobile.ViewTransition.Slide);
 	}
 
 });
